99c3c13b09404f93d600d97ffcc30f134c29d955
Bowarc <yuyeaaaahhhh@laposte.net>
2025-01-13 00:10:02 +0100

    Fixed windows scripts

--- scripts/watch_back.sh
+++ scripts/watch_back.sh
@@ -1,1 +1,1 @@
-cargo watch -s "cls && sh .\scripts\clean_back.sh & sh .\scripts\build_back.sh && cargo r -p back" -w ./back -w ./Rocket.toml --why
+cargo watch -s "clear && sh ./scripts/clean_back.sh & sh ./scripts/build_back.sh && cargo r -p back" -w ./back -w ./Rocket.toml --why
